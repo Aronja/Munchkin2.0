06efb87a9d3f80af9c3cb4196d17b47fb591a1a0
Aronja <anovicky@thoughtworks.com>
2020-01-14 21:29:17 +0000

    Change button, add player state

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Text, View, Image, Button} from 'react-native';
-import { styles } from "./styles";
+import {styles} from "./styles";
 
 class Welcome extends Component {
 
   render() {
     return (
-      <View style={{ alignItems: "center" }}>
+      <View style={{alignItems: "center"}}>
         <Text style={styles.header}>Munchkin Counter</Text>
         <Text>{"\n"}</Text>
         <Image
-          style={{ maxHeight: 285, maxWidth: 200 }}
+          style={{maxHeight: 285, maxWidth: 200}}
           source={require("./img/munchkin.jpg")}
         />
         <Text>{"\n"}</Text>
@@ -20,10 +20,19 @@ class Welcome extends Component {
 }
 
 class Game extends Component {
+
+  state = {players: {"name": "Astrid", "points": 0}};
+
   render() {
     return (
-      <View style={{ alignItems: "center" }}>
+      <View style={{alignItems: "center"}}>
         <Text style={styles.header}>Game</Text>
+        <Text style={styles.header}>{this.state.players["name"]} {this.state.players["points"]}</Text>
+        <Button
+          title="add player"
+          color="#841584"
+          onPress={this.toggleGame}
+        />
       </View>
     );
   }
@@ -32,21 +41,27 @@ class Game extends Component {
 export default class App extends Component {
 
   toggleGame = () => {
-    this.setState(prevState => ({isPlaying: !prevState.isPlaying }));
+    this.setState(prevState => ({isPlaying: !prevState.isPlaying}));
   };
 
-  state = {isPlaying : true};
+  state = {isPlaying: true};
 
   render() {
     const isPlaying = this.state.isPlaying;
     return (
       <View style={styles.container}>
         {isPlaying ? <Welcome/> : <Game/>}
-        <Button
-          title="start playing"
-          color="#841584"
-          onPress = {this.toggleGame}
-        />
+        {isPlaying ?
+          <Button
+            title="start playing"
+            color="#841584"
+            onPress={this.toggleGame}
+          /> :
+          <Button
+            title="go back"
+            color="#841584"
+            onPress={this.toggleGame}
+          />}
       </View>
 
     );
